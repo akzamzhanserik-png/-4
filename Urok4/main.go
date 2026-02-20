49582a1014c39fe8ffb566109ca8b3d2382f1e2c
akzamzhanserik-png <akzamzhanserik@gmail.com>
2026-02-20 23:01:38 +0500

    Add Urok4 Go task

---
+++ Urok4/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Я сегодня изучил:")
+	fmt.Println("Основы языка Go")
+
+	fmt.Println("Моя любимая песня: Mamasita ")
+	fmt.Println("Исполнитель: Jah Khalib")
+
+}
